49ac1b632e87044ecdeaf5c0c64d4720a74e0689
tomas.slavik <tomas.slavik@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-29 22:25:29 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/custom-account-fields/tags/1.1@975525 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ custom-account-fields.php
@@ -0,0 +1,177 @@
+<?php
+/*
+Plugin Name: Custom account fields
+Plugin URI: 
+Description: Doplňuje nové a nastavuje existující položky pro zákaznický (uživatelský) účet pro woocommerce. České a slovenské IČ (IČO), DIČ, slovenské IČ DPH nastavení telefonu jako nepovinné položky pro woocommerce. Adds new and sets existing customer account fields for woocommerce. Czech and Slovak IČ(IČO) - Company number, DIČ - VAT number, Slovak IČ DPH - VAT number 2 and phone number isn't required for woocommerce.
+Author: Tomáš Slavík
+Author URI: http://www.monitom.cz/
+Version: 1.1
+License: GPLv3 or later
+*/
+
+
+if ( !defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) 
+	return; // Check if WooCommerce is active
+
+if ( !class_exists( 'mtCustomAccountFields' ) ) :
+
+final class mtCustomAccountFields {
+
+	/**
+	 * @var string
+	 */
+	public $version = '1.1';
+
+	/**
+	 * @The single instance of the class
+	 */
+	protected static $_instance = null;
+
+	/**
+	 * Instance
+	 *
+	 * Ensures only one instance of class is loaded or can be loaded.
+	 *
+	 * @static
+	 * @return mtCustomAccountFieldse instance
+	 */
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * Cloning is forbidden.
+	 */
+	public function __clone() {
+		_doing_it_wrong( __FUNCTION__, __( 'Cloning is forbidden', 'custom-account-fields' ) );
+	}
+
+	/**
+	 * Unserializing instances of this class is forbidden.
+	 */
+	public function __wakeup() {
+		_doing_it_wrong( __FUNCTION__, __( 'Unserializing instances of this class is forbidden.', 'custom-account-fields') );
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		
+		load_plugin_textdomain( 'custom-account-fields', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		
+		// hooks
+		add_filter( 'woocommerce_billing_fields' , array( $this, 'woocommerce_billing_fields' ), 10, 2 );
+		add_filter( 'woocommerce_my_account_my_address_formatted_address', array( $this, 'woocommerce_my_account_my_address_formatted_address' ), 10, 3 );
+		add_filter( 'woocommerce_localisation_address_formats', array( $this, 'woocommerce_localisation_address_formats' ) );
+		add_filter( 'woocommerce_formatted_address_replacements', array( $this, 'woocommerce_formatted_address_replacements' ), 10, 2 );
+		add_filter( 'woocommerce_order_formatted_billing_address', array( $this, 'woocommerce_order_formatted_billing_address' ), 10, 2 );
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
+				
+	}
+	
+	public function woocommerce_billing_fields( $fields, $country ) {
+		// add company identification number (ico) 
+		$fields['billing_company_number'] = array(
+		'label'     => __('Company number', 'custom-account-fields'),
+		'placeholder'   => '',
+		'required'  => false,
+		'class'     => array('form-row-wide'),
+		'clear'     => true
+		 );
+
+		// add vat_number (dic)
+		$fields['billing_vat_number'] = array(
+		'label'     => __('VAT number', 'custom-account-fields'),
+		'placeholder'   => '',
+		'required'  => false,
+		'class'     => array('form-row-wide'),
+		'clear'     => true
+		 );
+
+//		if ($country === 'SK')
+		{
+			// add vat_number 2 (ic dph)
+			$fields['billing_vat_number_2'] = array(
+			'label'     => __('VAT number 2', 'custom-account-fields'),
+			'placeholder'   => '',
+			'required'  => false,
+			'class'     => array('form-row-wide'),
+			'clear'     => true
+			 );
+		}		
+		// change phone item required state
+		$fields['billing_phone']['required'] = false;
+
+		// Enqueue scripts
+		wp_enqueue_script( 'caf-address' );
+		 
+		return $fields;
+	}
+
+	public function woocommerce_my_account_my_address_formatted_address( $fields, $customer_id, $name ) {
+		return $fields += array(
+			'company_number' => get_user_meta( $customer_id, $name . '_company_number', true ),
+			'vat_number' => get_user_meta( $customer_id, $name . '_vat_number', true ),
+			'vat_number_2' => get_user_meta( $customer_id, $name . '_vat_number_2', true ));
+	}
+
+	public function woocommerce_localisation_address_formats($address_formats) {
+		$address_formats['CZ'] .= "\n{company_number}\n{vat_number}";
+		$address_formats['SK'] .= "\n{company_number}\n{vat_number}\n{vat_number_2}";
+		return $address_formats;
+	}
+
+	public function woocommerce_formatted_address_replacements( $replace, $args) {
+		extract( $args );
+		return $replace += array(
+			'{company_number}' => $company_number == '' ? '' : __('Company number: ', 'custom-account-fields') . $company_number,
+			'{vat_number}' => $vat_number == '' ? '' : __('VAT number: ', 'custom-account-fields') . $vat_number,
+			'{vat_number_2}' => $vat_number_2 == '' ? '' : __('VAT number 2: ', 'custom-account-fields') . $vat_number_2,
+			'{company_number_upper}' => strtoupper($company_number == '' ? '' : __('Company number: ', 'custom-account-fields') . $company_number),
+			'{vat_number_upper}' => strtoupper($vat_number == '' ? '' : __('VAT number: ', 'custom-account-fields') . $vat_number),
+			'{vat_number_upper_2}' => strtoupper($vat_number_2 == '' ? '' : __('VAT number 2: ', 'custom-account-fields') . $vat_number_2),
+		);
+	}
+
+	public function woocommerce_order_formatted_billing_address($address, $order) {
+		return $address += array(
+					'company_number'	=> $order->billing_company_number,
+					'vat_number'	=> $order->billing_vat_number,
+					'vat_number_2'	=> $order->billing_vat_number_2);
+	}
+	
+	public function plugin_url() {
+		return untrailingslashit( plugins_url( '/', __FILE__ ) );
+	}
+
+
+	public function wp_enqueue_scripts() {
+
+		$suffix               = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+		$assets_path          = str_replace( array( 'http:', 'https:' ), '', $this->plugin_url() ) . '/assets/';
+		$frontend_script_path = $assets_path . 'js/frontend/';
+
+		// Register any scripts for later use, or used as dependencies
+		wp_register_script( 'caf-address', $frontend_script_path . 'address' . $suffix . '.js', array( 'jquery' ), $this->version, true );
+		
+		// Queue frontend scripts conditionally	
+		if ( is_checkout() ) {
+			wp_enqueue_script( 'caf-address' );
+		}
+		
+		// Variables for JS scripts
+
+	}
+}	
+endif;
+
+mtCustomAccountFields::instance();
+
+?>
---
+++ readme.txt
@@ -0,0 +1,63 @@
+=== Custom account fields ===
+Contributors: Tomáš Slavík
+Donate link: mailto:custom-account-fields@monitom.cz
+Tags: woocommerce, DIČ, IČO, IČ, IČ DPH, česky, slovensky, česká, slovenská, české, slovenské, cz, sk, Czech, Slovak, zobrazení, úprava, VAT, number, Company, identification, tax, eshop, e-shop, ecommerce, e-commerce, commerce, woothemes, wordpress woocommerce, store, sales, sell, shop, shopping, cart, checkout, configurable, additional, fields, variable, download, downloadable, digital, inventory, fakturační, billing, shipping, adresa, address, woo commerce, order, objednávka
+Requires at least: 3.8 (WooCommerce: 2.1.0)
+Tested up to: 3.9.2 (2.1.12)
+Stable tag: 1.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Doplňuje nové a nastavuje existující položky zákaznického účtu pro woocommerce.
+Adds new and sets existing customer ... for woocommerce.
+
+== Description ==
+= CZ =
+
+Doplňuje nové a nastavuje existující položky pro zákaznický (uživatelský) účet pro woocommerce:
+
+ * České a slovenské IČ (IČO), DIČ 
+ * Slovenské IČ DPH (VAT number 2) pro Slovenskou republiku
+ * nastavení telefonu jako nepovinné položky pro woocommerce ve fakturační adrese.
+
+= EN =
+
+Adds new and sets existing customer account fields for woocommerce:
+
+ * Czech and Slovak IČ(IČO) - Company number, DIČ - VAT number 
+ * Slovak IČ DPH (VAT number 2) for Slovak republic
+ * phone number isn't required for woocommerce billing address.
+
+
+== Installation ==
+= CZ =
+1. Stáhněte si poslední verzi a rozbalte ji do adresáře /wp-content/plugins/, nebo plugin nainstalujte přes menu Pluginy -> Instalace pluginů v administraci.
+1. Aktivujte plugin přes menu Pluginy v administraci WordPressu.
+1. Hotovo!
+
+= EN =
+1. Upload the plugin to your web presentation (eshop) or install via plugin management.
+1. Activate the plugin through the 'Plugins' menu in WordPress administration
+1. Done!
+
+== Frequently Asked Questions ==
+V případě připomínek, nalezení chyby nás prosím kontaktujte následovně.
+
+Do you have questions or issues with this plugin? 
+
+1. [Support Forum](http://wordpress.org/support/plugin/custom-account-fields)
+1. [Support](mailto:custom-account-fields@monitom.cz)
+
+
+== Changelog ==
+= 1.1 =
+* CZ
+  * Přidáno IČ DPH pro slovenské firmy
+* EN
+  * Added IČ DPH for Slovak companies
+
+= 1.0 =
+* CZ
+  * Počáteční verze - IČ, DIČ, nepovinné pole Telefon
+* EN
+  * Initial version - IČ, DIČ, phone number isn't required
